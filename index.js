d4125aa8b664bafc14d6ecb4d7117f4378cd8349
SheikhKiron <sheikhkiron59@gmail.com>
2025-11-12 01:23:16 +0600

    cmnt some message

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const event = client.db('event');
     const eventCollection = event.collection('events');
 
@@ -43,7 +43,7 @@ async function run() {
 
       
       if (search) {
-        
+
         query.title = { $regex: search, $options: 'i' }; 
       }
 
@@ -154,7 +154,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
